112930e43152294ca9a0b5923fc252aafb4cb5de
unknown <NKarisa@KEPIL136160121.CI.ORG>
2017-03-07 00:31:24 +0300

    Updated install Sql

--- install/assets/install.sql
+++ install/assets/install.sql
@@ -5,84 +5,19 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `ci_sessions`;
-CREATE TABLE `ci_sessions` (
-  `id` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `ip_address` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
-  `data` blob NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `ci_sessions_timestamp` (`timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `accounts_id` int(10) NOT NULL AUTO_INCREMENT,
+  `numeric_code` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `opening_balance` decimal(10,2) NOT NULL,
+  `opening_date` date DEFAULT NULL,
+  PRIMARY KEY (`accounts_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-DROP TABLE IF EXISTS `language`;
-CREATE TABLE `language` (
-  `phrase_id` int(11) NOT NULL AUTO_INCREMENT,
-  `phrase` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `english` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `bengali` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `spanish` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `arabic` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `dutch` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `russian` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `chinese` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `turkish` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `portuguese` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `hungarian` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `french` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `greek` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `german` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `italian` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `thai` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `urdu` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `hindi` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `latin` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `indonesian` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `japanese` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `korean` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `swahili` longtext COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`phrase_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-DROP TABLE IF EXISTS `settings`;
-CREATE TABLE `settings` (
-  `settings_id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `description` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`settings_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-INSERT INTO `settings` (`settings_id`, `type`, `description`) VALUES
-(1,	'system_name',	'School Manager'),
-(2,	'system_title',	'School Manager'),
-(3,	'address',	'Kilifi'),
-(4,	'phone',	'+254711808071'),
-(5,	'paypal_email',	'admin@techsys.com'),
-(6,	'currency',	'Kes.'),
-(7,	'system_email',	'admin@techsys.com'),
-(20,	'active_sms_service',	'disabled'),
-(11,	'language',	'english'),
-(12,	'text_align',	'left-to-right'),
-(13,	'clickatell_user',	''),
-(14,	'clickatell_password',	''),
-(15,	'clickatell_api_id',	''),
-(16,	'skin_colour',	'Green'),
-(17,	'twilio_account_sid',	''),
-(18,	'twilio_auth_token',	''),
-(19,	'twilio_sender_phone_number',	'');
-
-
-DROP TABLE IF EXISTS `noticeboard`;
-CREATE TABLE `noticeboard` (
-  `notice_id` int(11) NOT NULL AUTO_INCREMENT,
-  `notice_title` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `notice` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `create_timestamp` int(11) NOT NULL,
-  PRIMARY KEY (`notice_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
+INSERT INTO `accounts` (`accounts_id`, `numeric_code`, `name`, `opening_balance`, `opening_date`) VALUES
+(1,	1,	'cash',	65000.00,	'2017-03-01'),
+(2,	2,	'bank',	342870.23,	'2017-03-01');
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
@@ -147,6 +82,62 @@ CREATE TABLE `budget_schedule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `cashbook`;
+CREATE TABLE `cashbook` (
+  `cashbook_id` int(200) NOT NULL AUTO_INCREMENT,
+  `batch_number` int(200) NOT NULL,
+  `t_date` date NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `transaction_type` int(10) NOT NULL COMMENT '1=income,2=expense,3=to_bank,4=to_cash',
+  `account` int(10) NOT NULL COMMENT '1=cash,2=bank',
+  `amount` decimal(10,2) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cashbook_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `ci_sessions`;
+CREATE TABLE `ci_sessions` (
+  `id` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `ip_address` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ci_sessions_timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `ci_sessions` (`id`, `ip_address`, `timestamp`, `data`) VALUES
+('17365dbcce04feb1566559c776308350cf5cc274',	'::1',	1488820936,	'__ci_last_regenerate|i:1488820619;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('2a322772c8882f1f2c47eebd39bf304280789dbd',	'::1',	1488833718,	'__ci_last_regenerate|i:1488833443;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('2a6b64bd2f1ee1927e4c2b4ade603e1f3997fa8d',	'::1',	1488824732,	'__ci_last_regenerate|i:1488824643;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('315208632a229b9d22aea84555469822c7cec053',	'::1',	1488831075,	'__ci_last_regenerate|i:1488830843;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('41be151926ebb47ba16de6c005c23b388790c9f1',	'::1',	1488822429,	'__ci_last_regenerate|i:1488822218;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('632405a75f2fb4a48bc2a3b8a043e9b34d01a05f',	'::1',	1488829280,	'__ci_last_regenerate|i:1488829050;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('668213ca80f9c4fb080cf4e5d0ad757123013f7a',	'::1',	1488833003,	'__ci_last_regenerate|i:1488832780;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('6b0419da0e9225989b5e6e693058eca7f984e8d4',	'::1',	1488835292,	'__ci_last_regenerate|i:1488835047;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('85c693474610180e1d50c002de2fef3fffc594df',	'::1',	1488835427,	'__ci_last_regenerate|i:1488835427;'),
+('884bde83e4a261738afb61379a5623809b92f70d',	'::1',	1488820449,	'__ci_last_regenerate|i:1488820188;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('89930bcf159f4c67925eee84860924777b03ad09',	'::1',	1488832437,	'__ci_last_regenerate|i:1488832437;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('8b2bbf15f2c2731ff9b6ea1cb22c5cc2c421536a',	'::1',	1488821845,	'__ci_last_regenerate|i:1488821657;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('8d3ea914cf142a467b2c987c2121b7e664bb3685',	'::1',	1488832391,	'__ci_last_regenerate|i:1488832123;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";flash_message|s:23:\"Data Added Successfully\";__ci_vars|a:1:{s:13:\"flash_message\";s:3:\"old\";}'),
+('8fafd67f9cd3b4fcf9e584798885037e4d8bd125',	'::1',	1488827708,	'__ci_last_regenerate|i:1488827417;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('a2185e1bd5728f46d15cce6ac6ce5095a0a4bb16',	'::1',	1488833947,	'__ci_last_regenerate|i:1488833868;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('a3b553cf65e4c54c36b37525ff4540288bd61dd9',	'::1',	1488834857,	'__ci_last_regenerate|i:1488834628;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('ad8d8cab3515f02b77b1581b6d7aaa99347df0cb',	'::1',	1488821485,	'__ci_last_regenerate|i:1488821250;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('b58521416e97059741348a3bc03f581ca3845d75',	'::1',	1488825841,	'__ci_last_regenerate|i:1488825835;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";flash_message|s:27:\"Expense Reversed Successful\";__ci_vars|a:1:{s:13:\"flash_message\";s:3:\"old\";}'),
+('b7000a6f701209f9adc2a53ae77957527f0bd994',	'::1',	1488823682,	'__ci_last_regenerate|i:1488823385;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('bf1913e0970ecf6ef44c3c57c7384bc6cdc78230',	'::1',	1488832076,	'__ci_last_regenerate|i:1488831814;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('c302c57e44f2f42f50a71bcb7fa22d72ff2a35e1',	'::1',	1488831378,	'__ci_last_regenerate|i:1488831147;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('c4ca5449b396d3e3ae39da678c05046a88ca780e',	'::1',	1488827156,	'__ci_last_regenerate|i:1488826873;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('cbb5372b4685f381b6e7e3b7749c1f2c937a1084',	'::1',	1488821196,	'__ci_last_regenerate|i:1488820940;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('d1bfce73a2a5482a4371b23c28e45c183bc1a95d',	'::1',	1488824363,	'__ci_last_regenerate|i:1488824165;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('d77e629fa53e9a214d553829c7223b32b8f16c0b',	'::1',	1488825644,	'__ci_last_regenerate|i:1488825178;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('df42f177d5e9feea640932114189f449e45f5398',	'::1',	1488822634,	'__ci_last_regenerate|i:1488822596;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('e60b3531b26d74fa2b4650ab5f4cbb4a71ebe4b0',	'::1',	1488833316,	'__ci_last_regenerate|i:1488833127;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('e766574dcc8a2ec78c0ac937f8d16048472c9e73',	'::1',	1488829578,	'__ci_last_regenerate|i:1488829551;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('ebd565084a84f3c55d8862295993ee93eee2e9cb',	'::1',	1488831758,	'__ci_last_regenerate|i:1488831500;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";'),
+('ec06c426ca101845cccfe3a0c2a8f347b13e226b',	'::1',	1488828025,	'__ci_last_regenerate|i:1488827769;admin_login|s:1:\"1\";admin_id|s:1:\"1\";login_user_id|s:1:\"1\";name|s:16:\"Nicodemus Karisa\";login_type|s:5:\"admin\";');
+
 DROP TABLE IF EXISTS `class`;
 CREATE TABLE `class` (
   `class_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -205,6 +196,20 @@ CREATE TABLE `exam` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
+DROP TABLE IF EXISTS `expense`;
+CREATE TABLE `expense` (
+  `expense_id` int(200) NOT NULL AUTO_INCREMENT,
+  `batch_number` int(10) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `payee` varchar(200) NOT NULL,
+  `t_date` date NOT NULL,
+  `method` varchar(20) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`expense_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `expense_category`;
 CREATE TABLE `expense_category` (
   `expense_category_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -212,6 +217,19 @@ CREATE TABLE `expense_category` (
   `income_category_id` int(10) NOT NULL,
   PRIMARY KEY (`expense_category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+DROP TABLE IF EXISTS `expense_details`;
+CREATE TABLE `expense_details` (
+  `expense_details_id` int(200) NOT NULL AUTO_INCREMENT,
+  `expense_id` int(200) NOT NULL,
+  `qty` int(50) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `unitcost` decimal(10,2) NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  `expense_category_id` int(200) NOT NULL,
+  PRIMARY KEY (`expense_details_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `fees_structure`;
@@ -282,6 +300,36 @@ CREATE TABLE `invoice_details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `language`;
+CREATE TABLE `language` (
+  `phrase_id` int(11) NOT NULL AUTO_INCREMENT,
+  `phrase` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `english` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `bengali` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `spanish` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `arabic` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `dutch` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `russian` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `chinese` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `turkish` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `portuguese` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `hungarian` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `french` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `greek` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `german` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `italian` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `thai` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `urdu` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `hindi` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `latin` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `indonesian` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `japanese` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `korean` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `swahili` longtext COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`phrase_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
 DROP TABLE IF EXISTS `mark`;
 CREATE TABLE `mark` (
   `mark_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -319,6 +367,16 @@ CREATE TABLE `message_thread` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
+DROP TABLE IF EXISTS `noticeboard`;
+CREATE TABLE `noticeboard` (
+  `notice_id` int(11) NOT NULL AUTO_INCREMENT,
+  `notice_title` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `notice` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `create_timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`notice_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
 DROP TABLE IF EXISTS `parent`;
 CREATE TABLE `parent` (
   `parent_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -336,6 +394,7 @@ CREATE TABLE `parent` (
 DROP TABLE IF EXISTS `payment`;
 CREATE TABLE `payment` (
   `payment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `batch_number` int(10) NOT NULL,
   `serial` int(10) NOT NULL,
   `detail_id` int(100) NOT NULL,
   `yr` int(10) NOT NULL,
@@ -359,6 +418,34 @@ CREATE TABLE `section` (
   PRIMARY KEY (`section_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+  `settings_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`settings_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `settings` (`settings_id`, `type`, `description`) VALUES
+(1,	'system_name',	'School Management System'),
+(2,	'system_title',	'School Management System'),
+(3,	'address',	'80-80200 Malindi'),
+(4,	'phone',	'254764837462'),
+(5,	'paypal_email',	'admin@techsys.com'),
+(6,	'currency',	'Kes.'),
+(7,	'system_email',	'mwambirenicodemus2017@gmail.com'),
+(20,	'active_sms_service',	'disabled'),
+(11,	'language',	'english'),
+(12,	'text_align',	'left-right'),
+(13,	'clickatell_user',	''),
+(14,	'clickatell_password',	''),
+(15,	'clickatell_api_id',	''),
+(16,	'skin_colour',	'Red'),
+(17,	'twilio_account_sid',	''),
+(18,	'twilio_auth_token',	''),
+(19,	'twilio_sender_phone_number',	''),
+(21,	'system_start_date',	'2017-03-01');
 
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
@@ -422,6 +509,10 @@ CREATE TABLE `terms` (
   PRIMARY KEY (`terms_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `terms` (`terms_id`, `term_number`, `name`) VALUES
+(1,	1,	'One'),
+(2,	2,	'Two'),
+(3,	3,	'Three');
 
 DROP TABLE IF EXISTS `transport`;
 CREATE TABLE `transport` (
@@ -434,4 +525,4 @@ CREATE TABLE `transport` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
--- 2017-01-30 14:06:44
+-- 2017-03-06 21:29:47
